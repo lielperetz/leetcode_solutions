bd116c3cf600c766eb8b5168959ec68b50a9a2d3
Liel Peretz <94789780+lielperetz@users.noreply.github.com>
2023-11-07 14:08:21 +0200

    Create eliminate-maximum-number-of-monsters.cs

---
+++ eliminate-maximum-number-of-monsters.cs
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/eliminate-maximum-number-of-monsters
+
+public class Solution {
+    public int EliminateMaximum(int[] dist, int[] speed) {
+        int n = dist.Length;
+
+        // Calculate the time required for each monster to reach the city
+        for (int i = 0; i < n; i++) {
+            dist[i] = (int)Math.Ceiling(dist[i] / (double)speed[i]);
+        }
+        Array.Sort(dist);
+
+        int x = 0;
+        // Check if we can continue to eliminate
+        for(int i = 0; i < n; i++) {
+            if(x >= dist[i]){
+                return i;
+            }
+            x++;
+        }
+        return n;
+    }
+}
